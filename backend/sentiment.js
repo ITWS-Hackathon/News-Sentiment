25dea3cf93d6d404f106ee96ba58050d52050196
RussellABH <rabh.2202@gmail.com>
2022-03-26 17:14:07 -0400

    Changed file name

--- backend/sentiment.js
+++ backend/sentiment.js
@@ -1,4 +1,4 @@
 const vader = require('vader-sentiment');
-const input = 'This movie sucked.';
+const input = 'This movie was good.';
 const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(input);
 console.log(intensity);
