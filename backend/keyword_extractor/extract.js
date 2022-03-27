e67cc2e20bf23bfee1f5c05020e5af29079839cd
Carter Ellis <carter.d.ellis10@gmail.com>
2022-03-27 14:51:00 -0400

    nav

--- backend/keyword_extractor/extract.js
+++ backend/keyword_extractor/extract.js
@@ -1,18 +1,15 @@
 // const express = require('express');
-// const router = express.Router(); 
+// const router = express.Router();
 
 // module.exports = router;
 
 // Test input
-let sentence = "Some interesting information about Ukraine and Russia"
+let sentence = "Some interesting information about Ukraine and Russia";
 
 //Usage: rake.generate(text-input, optional-stopwords)
 // const myStopwords = ['for', 'the', 'a', 'stands', 'test', 'man', 'woman'];
 // const opts = {stopwords: myStopwords};
 
-const rake = require('node-rake')
-const keywords = rake.generate(sentence)
-console.log(keywords)
-
-
-
+const rake = require("node-rake");
+const keywords = rake.generate(sentence);
+console.log(keywords);
