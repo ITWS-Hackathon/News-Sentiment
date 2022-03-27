644a778ef487bec9b5850929531cc6f49d055275
thedomino1313 <dominic.c.beyer@gmail.com>
2022-03-27 13:47:11 -0400

    git pull

--- backend/keywordExtract.js
+++ backend/keywordExtract.js
@@ -1,4 +1,4 @@
-module.exports = function(input){
+module.exports = function(input) {
 
     // example
     // const input = "hey there russia desert bomb strike on ukraine. This did not actually happen. This is just a test. This is just a test. Ukraine is cool. Ukraine loses to russia."
@@ -6,19 +6,14 @@ module.exports = function(input){
     const rake = require('node-rake');
     const keyphrases = rake.generate(input);
     const keywords = {};
-    for (let i = 0; i < keyphrases.length; i++){
+    for (let i = 0; i < keyphrases.length; i++) {
         let phrase = keyphrases[i].split(" ");
 
         //split words and add to json object
-        for (let j = 0; j < phrase.length; j++){
+        for (let j = 0; j < phrase.length; j++) {
             let word = phrase[j].toLowerCase()
-            if (!(word in keywords)) {keywords[word] = 1}
-            else keywords[word] += 1 ;
-        }     
+            if (!(word in keywords)) { keywords[word] = 1 } else keywords[word] += 1;
+        }
     }
     return keywords;
 }
-
-
-
-
--- backend/sentiment.js
+++ backend/sentiment.js
@@ -1,6 +1,5 @@
 module.exports = (theTexts) => {
-   const vader = require("vader-sentiment");
-   const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(theTexts);
-   return intensity.compound;
+    const vader = require("vader-sentiment");
+    const intensity = vader.SentimentIntensityAnalyzer.polarity_scores(theTexts);
+    return intensity.compound;
 }
-   
