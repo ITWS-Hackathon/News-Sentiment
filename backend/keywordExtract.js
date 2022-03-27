37201a9347f954c0adfdc8c52fab7b9253a9e668
Andy Ma <andyjma.work@gmail.com>
2022-03-27 11:30:43 -0400

    update extract keywords

--- backend/keywordExtract.js
+++ backend/keywordExtract.js
@@ -1,16 +1,24 @@
 module.exports = function(input){
-    const keyword_extractor = require("keyword-extractor");
-    //  Extract the keywords
-    const extraction_result =
-        keyword_extractor.extract(input,{
-        language:"english",
-        remove_digits: true,
-        return_changed_case:true,
-        remove_duplicates: false
 
-    });
-    return extraction_result;
+    // example
+    // const input = "hey there russia desert bomb strike on ukraine. This did not actually happen. This is just a test. This is just a test. Ukraine is cool. Ukraine loses to russia."
+
+    const rake = require('node-rake');
+    const keyphrases = rake.generate(input);
+    const keywords = {};
+    for (let i = 0; i < keyphrases.length; i++){
+        let phrase = keyphrases[i].split(" ");
+
+        //split words and add to json object
+        for (let j = 0; j < phrase.length; j++){
+            let word = phrase[j].toLowerCase()
+            if (!(word in keywords)) {keywords[word] = 1}
+            else keywords[word] += 1 ;
+        }     
+    }
+    return keywords;
 }
+
 
 
 
